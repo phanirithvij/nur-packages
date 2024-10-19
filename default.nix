940a1453a50377f2a4a02e1edfc80d3c48658761
phanirithvij <phanirithvij2000@gmail.com>
2024-10-20 00:38:38 +0530

    initial changes
    flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/600b15aea1b36eeb43833a50b0e96579147099ff?narHash=sha256-U9DDWMexN6o5Td2DznEgguh8TRIUnIl9levmit43GcI%3D' (2024-04-07)
      → 'github:NixOS/nixpkgs/ccc0c2126893dd20963580b6478d1a10a4512185?narHash=sha256-4HQI%2B6LsO3kpWTYuVGIzhJs1cetFcwT7quWCk/6rqeo%3D' (2024-10-18)
    
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- default.nix
+++ default.nix
@@ -6,15 +6,16 @@
 # commands such as:
 #     nix-build -A mypackage
 
-{ pkgs ? import <nixpkgs> { } }:
-
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  inherit (pkgs) callPackage;
+in
 {
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
 }
--- flake.nix
+++ flake.nix
@@ -1,22 +1,29 @@
 {
   description = "My personal NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       systems = [
         "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "armv6l-linux"
-        "armv7l-linux"
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+      pkgs = forAllSystems (system: import nixpkgs { inherit system; });
     in
     {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
+      legacyPackages = forAllSystems (
+        system:
+        import ./default.nix {
+          pkgs = pkgs.${system};
+        }
+      );
+      packages = forAllSystems (
+        system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system}
+      );
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.mkShellNoCC {
+          packages = [ pkgs.${system}.nixfmt-rfc-style ];
+        };
       });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
     };
 }
