ea016f53f9904e5bb92386b6dc59fc4d21014fca
phanirithvij <phanirithvij2000@gmail.com>
2024-12-17 21:51:27 +0530

    nix-schema: system arg
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/nix-schema.nix
+++ pkgs/nix-schema.nix
@@ -1,7 +1,10 @@
 # upd-all.sh manages the hash
 # https://github.com/gvolpe/nix-config/blob/3d2744a4d663d30f1b0b4014917f8d51b006f053/lib/overlays.nix#L87
+{
+  system ? "x86_64-linux",
+}:
 (builtins.getFlake "github:DeterminateSystems/nix-src/e1ed2d111bd21724f28ebb80aedd5154d4979fa9")
-.packages."x86_64-linux".nix.overrideAttrs
+.packages.${system}.nix.overrideAttrs
   (old: {
     doCheck = false;
     doInstallCheck = false;
