3ae9b89c8481fba3c9977f81a41dc02f85fb091c
phanirithvij <phanirithvij2000@gmail.com>
2025-01-04 20:30:32 +0530

    tcount: fix cargoHash and upd
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/unstable/tcount/package.nix
+++ pkgs/unstable/tcount/package.nix
@@ -1,9 +1,10 @@
 {
+  lib,
   fetchFromGitHub,
   tcount,
 }:
 tcount.overrideAttrs (
-  finalAttrs: _: {
+  finalAttrs: oldAttrs: {
     version = "master";
     src = fetchFromGitHub {
       owner = "rrethy";
@@ -11,6 +12,12 @@ tcount.overrideAttrs (
       rev = "refs/heads/${finalAttrs.version}";
       hash = "sha256-M4EvaX9qDBYeapeegp6Ki7FJLFosVR1B42QRAh5Eugo=";
     };
-    cargoHash = "sha256-Sn7hu+2jQFd2u8tpfTxnEO+syrO96gfgz6ouHxJnpLg=";
+    cargoDeps = oldAttrs.cargoDeps.overrideAttrs (
+      lib.const {
+        inherit (finalAttrs) src;
+        outputHash = "sha256-aUBGG/BD2ys2O50MuFlOOH4J+s0SfnuVAkYS9E4bQzA=";
+        outputHashMode = "recursive";
+      }
+    );
   }
 )
