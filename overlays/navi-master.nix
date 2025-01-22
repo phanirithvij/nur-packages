d352753eae0c6eabe9f8ce28840e133611a8a382
phanirithvij <phanirithvij2000@gmail.com>
2025-01-22 17:22:12 +0530

    navi-master: upd hash
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- overlays/navi-master.nix
+++ overlays/navi-master.nix
@@ -6,13 +6,13 @@ _: p: {
       owner = "denisidoro";
       repo = "navi";
       rev = "refs/heads/${version}";
-      hash = "sha256-8et38qn2ywKfaSxHSgAuMqcV+48nogfbMQgCzSB1bIg=";
+      hash = "sha256-TaB5opb7al+33qIrK/EPhkNChyr96I+9R1euU4bDj38=";
     };
     cargoDeps = old.cargoDeps.overrideAttrs (
       p.lib.const {
         name = "${pname}-vendor.tar.gz";
         inherit src;
-        outputHash = "sha256-vNfcSHNP0KNM884DMtraYohLOvumSZnEtemJ+bJSQ5o=";
+        outputHash = "sha256-G1HGpx4Df0aAtb4sx3HLI4iyvF5DBEylt2tFMjN1XSc=";
       }
     );
   });
