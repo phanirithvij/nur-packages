656ac4a3a405e268aab8fb1f31eabd94f7f6a76e
phanirithvij <phanirithvij2000@gmail.com>
2025-02-23 18:50:58 +0530

    slight refactor
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/nix-schema.nix
+++ pkgs/nix-schema.nix
@@ -3,15 +3,18 @@
 {
   system ? "x86_64-linux",
 }:
-(builtins.getFlake "github:DeterminateSystems/nix-src/9a4502bf3840efb259de3cc1cadaf747cc625d12")
-.packages.${system}.nix.overrideAttrs
-  (old: {
-    doCheck = false;
-    doInstallCheck = false;
-    postInstall =
-      (old.postInstall or "")
-      + ''
-        rm $out/bin/nix-*
-        mv $out/bin/nix $out/bin/nix-schema
-      '';
-  })
+let
+  flake = builtins.getFlake "github:DeterminateSystems/nix-src/9a4502bf3840efb259de3cc1cadaf747cc625d12";
+  # TODO, withAWS = false;
+  pkg = flake.packages.${system}.nix;
+in
+pkg.overrideAttrs (o: {
+  doCheck = false;
+  doInstallCheck = false;
+  postInstall =
+    (o.postInstall or "")
+    + ''
+      rm $out/bin/nix-*
+      mv $out/bin/nix $out/bin/nix-schema
+    '';
+})
