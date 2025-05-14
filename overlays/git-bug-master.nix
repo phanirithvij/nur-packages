61ee2a95b3e0839170ef1da73763577b83cc974f
phanirithvij <phanirithvij2000@gmail.com>
2025-05-14 18:07:13 +0530

    git-bug upd
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- overlays/git-bug-master.nix
+++ overlays/git-bug-master.nix
@@ -11,9 +11,9 @@ _: p: {
             owner = "git-bug";
             repo = "git-bug";
             rev = "refs/heads/${version}";
-            hash = "sha256-dn7fl7LO3t9517+5wmSMki5Ybs+crDfgjQFTe35HUns=";
+            hash = "sha256-9DKusxAwpPImkY211YI41aeIr15MC7KOFv2RSeGseyg=";
           };
-          vendorHash = "sha256-N4/4u1qc/gIwTGHtbxeVKZEKmbC/nm36v63l8apV0nw=";
+          vendorHash = "sha256-z9StU5cvZlDkmC7TE6JOhpxAx5oSTxAQTBh1LEksKww=";
           ldflags = [
             "-s"
             "-X github.com/MichaelMure/git-bug/commands.GitCommit=${version}"
