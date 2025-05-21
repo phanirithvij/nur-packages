afd4fd32bfef6bd67b26c1ec0d1302e7f011be22
phanirithvij <phanirithvij2000@gmail.com>
2025-05-21 21:29:55 +0530

    tgrclone: unbreak
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/tgrclone.nix
+++ pkgs/tgrclone.nix
@@ -5,13 +5,14 @@ rclone.overrideAttrs (
     # TODO unstable date so I can update
     version = "tgdrive-main";
     doInstallCheck = false;
+    patches = [ ]; # no patches
     src = fetchFromGitHub {
       owner = "tgdrive";
       repo = "rclone";
       rev = "refs/heads/main";
-      hash = "sha256-pQ+bRCw6A2TLzQduY7dIqL+xtXdkXP7Ch0OxyDxRW50=";
+      hash = "sha256-WlhJh7hqZAF0c5vQ9+vvG/cNDz4BGqbAQJXuwssytdY=";
     };
-    vendorHash = "sha256-Mdh6/4B/B/JsdMwO/1aopuJHE4020nYtBC4GdvdKJvg=";
+    vendorHash = "sha256-Nt+KFhOfY1z/q6xVJZ/aQvBWOwr7txkansF6PKxGdSY=";
     ldFlags = [
       "-s"
       "-w"
