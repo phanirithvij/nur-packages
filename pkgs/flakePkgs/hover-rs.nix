d364bc8c23dcbb1e21f436d9bcc1594e6768c01b
phanirithvij <phanirithvij2000@gmail.com>
2025-05-24 10:10:43 +0530

    hover-rs not available for aarch64-linux
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/flakePkgs/hover-rs.nix
+++ pkgs/flakePkgs/hover-rs.nix
@@ -4,4 +4,4 @@
 let
   f = builtins.getFlake "github:viperML/hover-rs/7a699b1e8a52c416e6d113a000b500752a6c3371";
 in
-f.packages.${system}.default
+if system == "aarch64-linux" then null else f.packages.${system}.default
