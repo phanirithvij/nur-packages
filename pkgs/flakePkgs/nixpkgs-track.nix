097065cd56b7926a94c10368573da1c4acfbf328
phanirithvij <phanirithvij2000@gmail.com>
2025-07-03 00:14:06 +0530

    fix nixpkgs-track package
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/flakePkgs/nixpkgs-track.nix
+++ pkgs/flakePkgs/nixpkgs-track.nix
@@ -4,4 +4,7 @@
 let
   f = builtins.getFlake "github:uncenter/nixpkgs-track/02c9aa6ecc63b30efb0bd388ac9d0c7b38f4f5e9";
 in
-f.packages.${system}.default
+f.packages.${system}.default.overrideAttrs (_: {
+  pname = "nixpkgs-track";
+  meta.mainProgram = "nixpkgs-track";
+})
