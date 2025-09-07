728d51f7cd336dd1aaa3f7744df40cdeb3df197a
phanirithvij <phanirithvij2000@gmail.com>
2025-09-07 23:57:20 +0530

    upd lazyjj temppatch
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/unstable/lazyjj/package.nix
+++ pkgs/unstable/lazyjj/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   fetchFromGitHub,
+  fetchpatch2,
   makeWrapper,
   jujutsu,
   rustPlatform,
@@ -18,7 +19,25 @@ rustPlatform.buildRustPackage {
     hash = "sha256-tTa34TeCXhJTy//SIisVqt2bfaGzNKho+AumHEXyzJ0=";
   };
 
-  cargoHash = "sha256-bQNLhQAUw2JgThC+RiNC5ap8D6a4JgflV2whXKu7QF8=";
+  cargoHash = "sha256-ytoSLy3HfG2rxpvXm9p2tQijPqO1lmto4eZQFgcpKL0=";
+
+  patches = [
+    # temporary breakage
+    # https://github.com/NixOS/nixpkgs/issues/370890
+    (fetchpatch2 {
+      url = "https://github.com/Cretezy/lazyjj/pull/159.patch?full_index=1";
+      hash = "sha256-HUyZzIaG7hR50zhY9t53Tzt+gMgJJYwLEQL3bpj/eKM=";
+    })
+  ];
+
+  cargoPatches = [
+    # TODO patch should be in nixpkgs, will not be merged upstream
+    # https://github.com/Cretezy/lazyjj/pull/117#issuecomment-3110844786
+    (fetchpatch2 {
+      url = "https://github.com/Cretezy/lazyjj/pull/117.patch?full_index=1";
+      hash = "sha256-fjH0PPu0+GpHdc+iR6aXMBm4TF5khuLJ1MViNNaEHcU=";
+    })
+  ];
 
   postInstall = ''
     wrapProgram $out/bin/lazyjj \
