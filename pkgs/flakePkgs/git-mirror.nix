d81bbf0933069a96b5ec14be14a7e56e8b7f80ce
phanirithvij <phanirithvij2000@gmail.com>
2025-10-24 16:57:32 +0530

    try fix git-mirror breakage on nod
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/flakePkgs/git-mirror.nix
+++ pkgs/flakePkgs/git-mirror.nix
@@ -4,4 +4,4 @@
 let
   f = builtins.getFlake "github:bachp/git-mirror/27235dcb08cc1b3011f2d816aaf76d7ee530a9c3";
 in
-f.packages.${system}.default
+if system != "x86_64-linux" then null else f.packages.${system}.default
