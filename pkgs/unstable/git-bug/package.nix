fa504c60b6c55b1b96364d735a45bf45ddb87c3e
phanirithvij <phanirithvij2000@gmail.com>
2026-02-15 12:10:13 +0530

    git-bug: simplify override
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/unstable/git-bug/package.nix
+++ pkgs/unstable/git-bug/package.nix
@@ -1,29 +1,16 @@
 {
   git-bug,
-  buildGoModule,
   fetchFromGitHub,
 }:
-git-bug.override {
-  buildGoModule =
-    args:
-    buildGoModule (
-      args
-      // rec {
-        pname = "git-bug";
-        version = "0.10.1-unstable-2026-01-27";
-        src = fetchFromGitHub {
-          owner = "git-bug";
-          repo = "git-bug";
-          rev = "0d58c4961980536c6e33638919ebac52d58bc757";
-          hash = "sha256-72yg5BZwsxyk2SwnpSJDKDoECWTLB/7VwoPYu48tLEE=";
-        };
-        vendorHash = "sha256-n/95OQVHqibb2dm04KyR5mPlSWqDlb0nAW5J/zASf3g=";
-        ldflags = [
-          "-s"
-          "-X github.com/MichaelMure/git-bug/commands.GitCommit=${version}"
-          "-X github.com/MichaelMure/git-bug/commands.GitLastTag=${version}"
-          "-X github.com/MichaelMure/git-bug/commands.GitExactTag=${version}"
-        ];
-      }
-    );
-}
+git-bug.overrideAttrs (prevAttrs: {
+  pname = "git-bug";
+  version = "0.10.1-unstable-2026-01-27";
+  src = fetchFromGitHub {
+    owner = "git-bug";
+    repo = "git-bug";
+    rev = "0d58c4961980536c6e33638919ebac52d58bc757";
+    hash = "sha256-72yg5BZwsxyk2SwnpSJDKDoECWTLB/7VwoPYu48tLEE=";
+  };
+  vendorHash = "sha256-n/95OQVHqibb2dm04KyR5mPlSWqDlb0nAW5J/zASf3g=";
+  ldflags = (prevAttrs.ldflags or [ ]) ++ [ "-s" ];
+})
