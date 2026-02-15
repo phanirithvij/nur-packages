d6e452a8b09aa6fc88e1b8ca33092f086106805f
phanirithvij <phanirithvij2000@gmail.com>
2026-02-15 12:44:20 +0530

    tgrclone: fix build
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/unstable/tgrclone/package.nix
+++ pkgs/unstable/tgrclone/package.nix
@@ -1,18 +1,24 @@
-{ rclone, fetchFromGitHub }:
+{
+  rclone,
+  fetchFromGitHub,
+}:
 rclone.overrideAttrs (
   f: _: {
     pname = "rclone";
     version = "1.73.1-unstable-2026-02-04";
-    doInstallCheck = false;
-    patches = [ ]; # no patches
-    src = fetchFromGitHub {
-      owner = "tgdrive";
-      repo = "rclone";
-      rev = "cf4e39c6703245dc66b5bfc0cc30821f95d2434f";
-      hash = "sha256-xGSMpQahCkzWKaQtM/MPZw5tCM1ZqNe9IMEnMjkn4i4=";
-    };
+    src =
+      (fetchFromGitHub {
+        owner = "tgdrive";
+        repo = "rclone";
+        rev = "cf4e39c6703245dc66b5bfc0cc30821f95d2434f";
+        hash = "sha256-xGSMpQahCkzWKaQtM/MPZw5tCM1ZqNe9IMEnMjkn4i4=";
+      })
+      // {
+        tag = "faketag-to-allow-eval";
+      };
     vendorHash = "sha256-LomeLlk0d/HTL0NKmbd083u7BHsy4FmAah9IzvmtO2s=";
-    ldFlags = [
+    doInstallCheck = false;
+    ldflags = [
       "-s"
       "-X github.com/rclone/rclone/fs.Version=${f.version}"
     ];
