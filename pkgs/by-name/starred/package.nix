9af04a7a10c002b0d2f5d20b00e3f9ed9d4ada5f
phanirithvij <phanirithvij2000@gmail.com>
2026-07-26 21:04:59 +0530

    starred: update
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/starred/package.nix
+++ pkgs/by-name/starred/package.nix
@@ -5,26 +5,25 @@
 }:
 # Copied from https://github.com/jpetrucciani/nix/blob/2304bf8492c166753df7e3bcef8a801c73f0582c/mods/python/misc.nix#L175
 # Licensed MIT Copyright (c) 2023 Jacobi Petrucciani
-python3.pkgs.buildPythonPackage rec {
+python3.pkgs.buildPythonPackage (finalAttrs: {
   pname = "starred";
-  version = "4.2.0";
+  version = "4.3.0-unstable-2026-06-07";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "maguowei";
-    repo = pname;
-    rev = "v${version}";
-    hash = "sha256-ugseXFiDQXLCg9wImpLCPmRJp31/OI8VuxxYD4JJ8mg=";
+    repo = "starred";
+    rev = "1e2bb94f44c376e06f8515d7304404ef1c93d4ca";
+    hash = "sha256-+sSly81YRI8hcQHeXNNoSS6Q4U0/Y4S0psisKh5IVW8=";
   };
 
-  build-system = with python3.pkgs; [ setuptools ];
+  build-system = with python3.pkgs; [ hatchling ];
 
   propagatedBuildInputs = with python3.pkgs; [
     aiohttp
     click
     github3-py
     gql
-    poetry-core
     requests
   ];
 
@@ -40,4 +39,4 @@ python3.pkgs.buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ phanirithvij ];
   };
-}
+})
