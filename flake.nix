559afe910c0ba5fe8a026a83125762c05a7411e0
phanirithvij <phanirithvij2000@gmail.com>
2026-09-05 13:10:58 +0530

    begone foul demon
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       systems = [
         "x86_64-linux"
         "aarch64-linux"
-        "x86_64-darwin"
+        #"x86_64-darwin"
         "aarch64-darwin"
       ];
       forAllSystems = f: inputs.nixpkgs.lib.genAttrs systems f;
