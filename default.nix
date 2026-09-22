f0c80b8caa6a23bee31a75ae78f9c17647d4ff6f
phanirithvij <phanirithvij2000@gmail.com>
2026-09-22 11:21:04 +0530

    flake: update inputs
    Flake lock file updates:
    
    • Updated input 'nix-update':
    
    'github:Mic92/nix-update/4f9f53413ba6e8b19de1b3a0500f17910320eda4?narHash=sha256-IZYs7aT8IQH%2BCFnMV2SLgmkaZ%2BUzQr1XqA%2BF6GQOD%2BY%3D'
    (2026-09-02)
      →
    'github:Mic92/nix-update/d23a574edb7560505af9ccb472d0baeec5aae130?narHash=sha256-/wrffGP0Jnf5JaYPtHPt%2BnpzrrRW2lhfAHberItn2zM%3D'
    (2026-09-20)
    • Updated input 'nixpkgs':
    
    'github:phanirithvij/nixpkgs/1ec21e459024623db649293f9de301302fcb5908?narHash=sha256-oJPs4OBZkFb7JfCEBWxD0tvS66lsUGVZCxBGC6dtcf8%3D'
    (2026-09-05)
      →
    'github:phanirithvij/nixpkgs/834e5749524c3768af3fd64a0bae9aae62ca1791?narHash=sha256-72CPkKPesNtCuJPnznKMXkFdq2feDk7IBNVAZ1uE4bI%3D'
    (2026-09-22)

--- default.nix
+++ default.nix
@@ -53,7 +53,6 @@ let
     keepassxc = callPackage ./pkgs/patched/keepassxc { };
     pr-tracker = callPackage ./pkgs/patched/pr-tracker { };
     gitbatch = callPackage ./pkgs/patched/gitbatch { };
-    lazygit = callPackage ./pkgs/patched/lazygit { };
 
     # cached packages
     # push em to cachix/oranc binary caches
--- pkgs/patched/lazygit/default.nix
+++
@@ -1,7 +0,0 @@
-{ lazygit }:
-lazygit.overrideAttrs (old: {
-  patches = (old.patches or [ ]) ++ [
-    # https://github.com/jesseduffield/lazygit/issues/5945
-    ./worktrees-search-branch.patch
-  ];
-})
